036d28741215ec9b7d4b240a8c815e8c4aff5c00
uih38891 <ruben.mazureac@continental-corporation.com>
2026-06-05 21:38:13 +0300

    fix bug

---
+++ src/pages/ProfileCreatePage.tsx
@@ -0,0 +1,9 @@
+import { ProfileCreator } from '@/components/profile/ProfileCreator';
+
+/**
+ * ProfileCreatePage - Create a new user profile.
+ * Requirements: 2.4, 2.5, 2.8
+ */
+export default function ProfileCreatePage() {
+  return <ProfileCreator />;
+}
